d045d26b47929bd0bfde233345e6a2f5721e5810
ash <ashddev@proton.me>
2025-01-03 15:39:50 +0100

    remove username and add id

--- user-service/src/main.py
+++ user-service/src/main.py
@@ -7,7 +7,7 @@ from typing_extensions import Annotated
 from src.models import User 
 from src.utils import create_access_token, create_refresh_token, hash_pass, verify_pass
 from src.db import create_db_and_tables, SessionDep
-from src.middleware import get_current_user, refresh_current_user
+from src.middleware import authorize_refresh, authorize_request
 
 
 @asynccontextmanager
@@ -22,39 +22,55 @@ async def root():
     return "Active"
 
 class Register(BaseModel):
-    username: str
-    password: Annotated[str, StringConstraints(min_length=8,max_length=20)]
     email: EmailStr
+    password: Annotated[str, StringConstraints(min_length=8,max_length=20)]
 
 @app.post("/register/")
 async def register(user: Register, session: SessionDep):
+    statement = select(User).where(User.email == user.email)
+    existing_user = session.exec(statement).first()
+    
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="An account with this email already exists.",
+        )
+    
     hash = hash_pass(user.password)
-    new_user = User(username=user.username, email=user.email, password=hash)
+    
+    new_user = User(email=user.email, password=hash)
     session.add(new_user)
     session.commit()
     session.refresh(new_user)
+    
     return new_user
 
+
 class Login(BaseModel):
-    username: str
-    password: str
+    email: EmailStr
+    password: Annotated[str, StringConstraints(min_length=8,max_length=20)]
 
 @app.post("/login/")
 async def login(login_request: Login, session: SessionDep):
-    user = session.exec(select(User).where(User.username == login_request.username)).first()
-    if not user or not verify_pass(login_request.password, user.password):
+    user = session.exec(select(User).where(User.email == login_request.email)).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="User does not exist")
+    if not verify_pass(login_request.password, user.password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
     
-    access_token = create_access_token({"sub": user.username})
-    refresh_token = create_refresh_token({"sub": user.username})
+    access_token = create_access_token({"sub": str(user.uuid)})
+    refresh_token = create_refresh_token({"sub": str(user.uuid)})
 
     return {"access_token": access_token, "refresh_token": refresh_token}
 
 @app.post("/refresh/")
-async def refresh_token(username: str = Depends(refresh_current_user)):    
-    access_token = create_access_token({"sub": username})
+async def refresh_token(uuid: str = Depends(authorize_refresh)):    
+    access_token = create_access_token({"sub": uuid})
     return {"access_token": access_token}
 
-@app.get("/protected/")
-async def protected_route(username: str = Depends(get_current_user)):
-    return {"message": "Access granted", "username": username}
+@app.get("/user/")
+async def get_user(session: SessionDep, uuid: str = Depends(authorize_request)):
+    statement = select(User).where(User.uuid == uuid)
+    user = session.exec(statement).first()
+
+    return {"user": {"id": user.uuid,"email": user.email, "verified": user.verified}}
--- user-service/src/models.py
+++ user-service/src/models.py
@@ -1,6 +1,13 @@
 from sqlmodel import Field, SQLModel
+import uuid as uuid_pkg
 
 class User(SQLModel, table=True):
-    username: str = Field(default=None, primary_key=True)
-    password: str = Field(default=None)
-    email: str = Field(index=True)
+    uuid: uuid_pkg.UUID = Field(
+        default_factory=uuid_pkg.uuid4,
+        primary_key=True,
+        index=True,
+        nullable=False,
+    )
+    email: str = Field(index=True, nullable=False, unique=True)
+    password: str = Field(nullable=False)
+    verified: bool = Field(default=False, nullable=False)
